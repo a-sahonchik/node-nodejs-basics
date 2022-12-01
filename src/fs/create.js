3a94a149d6531f93a0e1f4c584e67702741fbf85
Alexey Sahonchik <sahonchik.alexey@gmail.com>
2022-12-01 16:32:05 +0300

    feat: add file creation

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { writeFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = `${__dirname}/files/fresh.txt`;
+const errorMessage = "FS operation failed";
+const fileContent = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+    writeFile(filePath, fileContent, {
+        flag: 'ax'
+    }).catch(() => {
+        throw new Error(errorMessage)
+    });
 };
 
 await create();
