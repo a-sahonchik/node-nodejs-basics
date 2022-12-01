f2484e420253fd2510fa942b7b60fe553ac782f4
Alexey Sahonchik <sahonchik.alexey@gmail.com>
2022-12-01 20:50:04 +0300

    feat: add file deletion

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import { isFileOrFolderExists } from '../helpers/fsHelpers.js';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import { rm } from 'node:fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const fileToRemove = `${__dirname}/files/fileToRemove.txt`;
+    const errorMessage = "FS operation failed";
+
+    rm(fileToRemove)
+        .catch(() => {
+            throw new Error(errorMessage)
+        });
 };
 
 await remove();
