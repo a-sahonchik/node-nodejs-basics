c2b7878f00db9b746ff1f26edd716670bbd80ff5
Alexey Sahonchik <sahonchik.alexey@gmail.com>
2022-12-01 21:34:33 +0300

    feat: add env parsing

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envPrefix = 'RSS_';
+    let result = '';
+
+    Object.entries(process.env).map(([envName, envValue ]) => {
+        if (envName.includes(envPrefix)) {
+            result += `${envName}=${envValue}; `
+        }
+    });
+
+    console.log(result)
 };
 
 parseEnv();
