fde098dd1210c1c2ed76311c7796ed55ebf158c1
Alexey Sahonchik <sahonchik.alexey@gmail.com>
2022-12-02 16:00:32 +0300

    feat: add hash calculation

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { createReadStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const fileToRead = `${__dirname}/files/fileToCalculateHashFor.txt`;
+
+    const { createHash } = await import('node:crypto');
+    const hash = createHash('sha256');
+    const input = createReadStream(fileToRead);
+
+    try {
+        input.on('readable', () => {
+            const data = input.read();
+            if (data)
+                hash.update(data);
+            else {
+                console.log(hash.digest('hex'));
+            }
+        });
+    } catch (e) {
+        throw new Error(e);
+    }
 };
 
 await calculateHash();
