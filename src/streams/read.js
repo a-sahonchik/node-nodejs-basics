569f04e8f7f38d9ac5625a294a4fac0f68e0c0b7
Alexey Sahonchik <sahonchik.alexey@gmail.com>
2022-12-02 20:29:50 +0300

    feat: add file reading using readable stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,26 @@
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import fs from 'node:fs';
+import { stdout } from 'node:process';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const fileToRead = `${__dirname}/files/fileToRead.txt`;
+
+    const readStream = fs.createReadStream(fileToRead);
+
+    readStream.on('data', (chunk) => {
+        stdout.write(chunk)
+    });
+
+    readStream.on('end', () => {
+        stdout.write('\n')
+    });
+
+    readStream.on('error', (error) => {
+        throw error;
+    })
 };
 
 await read();
