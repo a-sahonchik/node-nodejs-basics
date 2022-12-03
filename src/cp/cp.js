519d6935cad38e512d0147ac6b0fd43093bd1c79
Alexey Sahonchik <sahonchik.alexey@gmail.com>
2022-12-03 20:46:22 +0300

    feat: add child process spawn with IPC-channel

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,28 @@
+import { fork } from 'node:child_process';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import { stdin, stdout } from 'node:process';
+
+const args = process.argv.slice(2);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    try {
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = dirname(__filename);
+        const childScriptPath = `${__dirname}/files/script.js`;
+
+        const child = fork(childScriptPath, args, { silent: true });
+
+        stdin.pipe(child.stdin);
+
+        child.stdout.pipe(stdout);
+
+        child.stdout.on("data", (data) => {
+            console.log(`Received from child process: ${data}`);
+        });
+    } catch (err) {
+        throw new Error(err);
+    }
 };
 
-spawnChildProcess();
+spawnChildProcess(args);
