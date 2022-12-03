ea39e405093b83182333b5b0412694b25e1cd0e4
Alexey Sahonchik <sahonchik.alexey@gmail.com>
2022-12-03 12:31:41 +0300

    feat: add text reversing using transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,22 @@
+import { Transform, pipeline } from 'node:stream';
+import { stdin, stdout } from "node:process";
+
 const transform = async () => {
-    // Write your code here 
+    const strReverse = new Transform({
+        transform(chunk, encoding, callback) {
+            const strChunk = chunk.toString().trim();
+            const reversedChunk = strChunk.toString().split('').reverse().join('');
+            this.push(`${reversedChunk}\n`);
+            callback();
+        }
+    });
+
+    pipeline(
+        stdin,
+        strReverse,
+        stdout,
+        (err) => console.error(err)
+    );
 };
 
 await transform();
